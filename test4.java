6ef0d31ea19678292d09e1553efc3affb4afc7e8
IASHW <senfoni_81@hotmail.com>
2022-11-07 00:17:23 +0300

    test4

---
+++ test4.java
@@ -0,0 +1,13 @@
+public class test4 {
+    public static void main(String[] args) {
+      byte vByte = 115;
+      short vShort = 20562;
+      int vInt = 6874613;
+
+      System.out.println(vByte);
+      System.out.println(vShort);
+      System.out.print(vInt);
+
+    }
+    
+}
