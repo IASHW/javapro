5bda613e964feff54da9a48676d2d26920c58ffa
IASHW <senfoni_81@hotmail.com>
2022-11-07 00:50:01 +0300

    test5

---
+++ test5.java
@@ -0,0 +1,15 @@
+public class test5 {
+    public static void main(String[] args) {
+        char vChar = 79;
+        char v2Char = 71;
+        char v3Char = 85;
+        char v4Char = 90;
+        char v5Char = 72;
+        char v6Char = 65;
+        char v7Char = 78;
+// cift tirnak kullanmadan yazdirirsek yukardaki degerlerin toplamini verir.
+        System.out.print("" + vChar + v2Char + v3Char + v4Char + v5Char + v6Char + v7Char);
+       
+    }
+    
+}
